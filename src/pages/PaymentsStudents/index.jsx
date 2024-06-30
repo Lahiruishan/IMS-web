9127808e2165eb71f553ebcd7ea7a0b877449a74
ishan lahiru <101960845+Lahiruishan@users.noreply.github.com>
2024-06-30 19:11:16 +0530

    commented
    add comments

--- src/pages/PaymentsStudents/index.jsx
+++ src/pages/PaymentsStudents/index.jsx
@@ -1,28 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import {  Text, Heading, Button } from "../../components";
+import {   Heading, Button } from "../../components";
 import IMA from "../../components/IMA";
 
 export default function PaymentsStudents() {
   return (
     <>
+    {/* Helmet is used to manage the document head */}
       <Helmet>
         <title>IMS-web</title>
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
+      {/* Main container with full width and gradient background */}
       <div className="w-full pb-[13px] bg-gradient">
         <div>
+
+          {/* IMA component with styles for flex layout */}
           <IMA className="flex md:flex-col justify-between items-start gap-5 bg-indigo-900" />
+           {/* Flex container for layout */}
           <div className="flex md:flex-col justify-center items-start gap-[49px]">
+            {/* Sidebar container */}
             <div className="h-[825px] w-[17%] md:w-full md:h-auto md:p-5 relative">
               <div className="flex flex-col items-center mt-[65px] ml-[41px] gap-[25px] md:ml-0">
-                
+               {/* Sidebar content can be added here */} 
               </div>
+               {/* Buttons container */}
               <div className="w-full h-max left-0 bottom-0 right-0 top-0 m-auto absolute">
                 <div>
                   <Button shape="square" className="w-full sm:px-5 z-[1] border-black-900 border border-solid">
                     Home
                   </Button>
+                    {/* Additional buttons */}
                   <div className="h-[52px] mt-[-49px] bg-indigo-100 shadow-bs" />
                 </div>
                 <div className="flex flex-col mt-[-52px] py-[52px] md:py-5 bg-indigo-300">
@@ -46,6 +54,7 @@ export default function PaymentsStudents() {
                  </div>
                 </div>
               </div>
+              {/* Main content container */}
               <div className="mt-8 flex items-start gap-[30px] sm:flex-col">
                 <div className="mt-[109px] flex flex-col gap-[21px]">
                     <Button size="6xl" as="p">
@@ -58,6 +67,7 @@ export default function PaymentsStudents() {
                         3.2026 A/L
                     </Button>
                 </div>
+                 {/* Heading for Student's Payments */}
                 <Heading size="11xl" as ="h3">Student's  Payments </Heading>
               </div>
             </div>
