c92535bdd65bc2b12f400300db7e31717683deef
ishan lahiru <101960845+Lahiruishan@users.noreply.github.com>
2024-06-30 19:17:54 +0530

    commented
    add comments

--- src/pages/PaymentsTeachers/index.jsx
+++ src/pages/PaymentsTeachers/index.jsx
@@ -6,17 +6,24 @@ import IMA from "../../components/IMA";
 export default function PaymentsTeachers() {
   return (
     <>
+     {/* Helmet is used to manage the document head */}
       <Helmet>
         <title>IMS-web</title>
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
+      {/* Main container with full width and gradient background */}
       <div className="w-full pb-[13px] bg-gradient">
         <div>
+           {/* IMA component with styles for flex layout */}
           <IMA className="flex md:flex-col justify-between items-start gap-5 bg-indigo-900" />
+          {/* Flex container for layout */}
           <div className="flex md:flex-col justify-center items-start gap-[49px]">
+            {/* Sidebar container */}
             <div className="h-[825px] w-[17%] md:w-full md:h-auto md:p-5 relative">
               <div className="flex flex-col items-center mt-[65px] ml-[41px] gap-[25px] md:ml-0">
+             {/* Sidebar content can be added here */}
               </div>
+               {/* Buttons container */}
               <div className="w-full h-max left-0 bottom-0 right-0 top-0 m-auto absolute">
                 <div>
                   <Button shape="square" className="w-full sm:px-5 z-[1] border-black-900 border border-solid">
@@ -24,6 +31,7 @@ export default function PaymentsTeachers() {
                   </Button>
                   <div className="h-[52px] mt-[-49px] bg-indigo-100 shadow-bs" />
                 </div>
+                {/* Additional buttons */}
                 <div className="flex flex-col mt-[-52px] py-[52px] md:py-5 bg-indigo-300">
                 <Button shape="square" className="w-full sm:px-5">
                     Teachers
@@ -52,12 +60,15 @@ export default function PaymentsTeachers() {
                  </div>
                 </div>
               </div>
+               {/* Main content container */}
               <div className="mt-8 flex items-start gap-[30px] sm:flex-col">
                 <div className="mt-[109px] flex flex-col gap-[21px]">
-                   
+                  {/* Buttons for different years can be added here */} 
                 </div>
+                 {/* Heading for Teacher's Payments */}
                 <Heading size="19xl" as ="h2">Teacher's  Payments </Heading>
                 </div>
+                 {/* Form container */}
               <div className="mt-[23px] flex flex-col items-end 5 self-stretch">
                 <div className="flex items-center justify-between gap-5 self-stretch sm:flex-col">
                 <Text size="5xl" as="p" className="mt-0.5">Name:</Text>
