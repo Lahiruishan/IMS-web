efa0d4e0840cd52858c6e6341620d0952fd58e69
ishan lahiru <101960845+Lahiruishan@users.noreply.github.com>
2024-06-30 18:54:59 +0530

    Update index.jsx
    commented

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -8,11 +8,17 @@ export default function LoginPage() {
 
   return (
     <>
+     {/* Helmet is used to manage the document head */}
     <Helmet>
       <title>IMS-web</title>
       <meta name="description" content="Web site created using create-react-app" />
     </Helmet>
+
+     {/* Main container with full width and gradient background */}
     <div className="w-full bg-gradient pb-3 pr-[59px] md:pr-5">
+
+      {/* Flex container for layout, with items centered and some gap between them */}
+     
     <div className="flex items-center gap -[50px] md:flex-col">
     <div className="flex w-[48%] flex-col items-center justify-center bg-indigo-900 pb-[558px] pl-14 pt-[297px] md:w-full md:p-5 md:py-5 md:pl-5">
     <Text size="vl" as="p" className="mb-4 ml-96 md:ml-0 !text-purple-50 !font-aclonica">
@@ -21,22 +27,23 @@ export default function LoginPage() {
    
       </Text>
     </div>
-
+{/* Right side of the page with login form */}
 <div className="flex flex-1 flex-col items-center justify-center rounded-[10px] bg-blue_gray-100 px-14 pb-[151px] pt-[68px] shadow-xs md:self-stretch md:p-5">
 <div className="flex w-[88%] flex-col items-start gap-[47px] md:w-full">
   <Text size="10xl" as="p" className="! font-actor underline">
     Login to your Account
   </Text>
-
+  {/* Login form */}
   <form action="" class="px-10">
-
+{/* Username field */}
   <div className="ml-6 flex flex-col items-start self-stretch md:ml-0">
   <label for="" class="text-neutral-1900 text-base font-normal ">User Name :</label>
   <input type="text" name="" placeholder="Enter your User Name " id=""
             class="w-full text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none" />
 
   <div className="mt-4 h-[42px] self-stretch rounded-[15px] bg-blue_gray-100"/>
-
+ 
+ {/* Password field */}
 <div className="mt-7 flex flex-col">
 <label for="" class="text-neutral-1900 text-base font-normal">Password :</label>
 <input type="text" name="" placeholder="Enter your Password " id=""
@@ -45,6 +52,7 @@ export default function LoginPage() {
 </div>
 <div className="mt-4 h-[42px] self-stretch rounded-[15px] bg-blue_gray-100"/>
 
+{/* Login button */}
 <div className="relative mt-[52px] h-[42px] w-[29%] self-center md:h-auto">
  <div>
   <a href="#" className="mr-6 md:mr-0">
