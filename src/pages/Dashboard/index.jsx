55c5b85d42366a0a379f14fe21f8d7420dc0f17a
ishan lahiru <101960845+Lahiruishan@users.noreply.github.com>
2024-06-30 21:49:42 +0530

    commented
    add comments

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,64 +1,131 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Text, Button } from "../../components";
+import { Img, Text, Heading, Button } from "../../components";
+import IMA from "../../components/IMA";
 
-export default function LoginPage() {
+export default function DashboardPage() {
   return (
     <>
-      {/* Helmet is used to manage the document head */}
+    {/* Helmet is used to manage the document head */}
       <Helmet>
         <title>IMS-web</title>
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
-
       {/* Main container with full width and gradient background */}
-      <div className="w-full bg-gradient pb-3 pr-[59px] md:pr-5">
-        {/* Flex container for layout, with items centered and some gap between them */}
-        <div className="flex items-center gap-[50px] md:flex-col">
-          {/* Left side of the page with text and styles */}
-          <div className="flex w-[48%] flex-col items-center justify-center bg-indigo-900 pb-[558px] pl-14 pt-[297px] md:w-full md:p-5 md:py-5 md:pl-5">
-            <Text size="vl" as="p" className="mb-4 ml-96 md:ml-0 !text-purple-50 !font-aclonica">
-              Institute <br /> Management <br /> System
-            </Text>
-          </div>
-
-          {/* Right side of the page with login form */}
-          <div className="flex flex-1 flex-col items-center justify-center rounded-[10px] bg-blue_gray-100 px-14 pb-[151px] pt-[68px] shadow-xs md:self-stretch md:p-5">
-            <div className="flex w-[88%] flex-col items-start gap-[47px] md:w-full">
-              <Text size="10xl" as="p" className="!font-actor underline">
-                Login to your Account
-              </Text>
-
-              {/* Login form */}
-              <form action="" className="px-10">
-                {/* Username field */}
-                <div className="ml-6 flex flex-col items-start self-stretch md:ml-0">
-                  <label for="" className="text-neutral-1900 text-base font-normal">User Name :</label>
-                  <input type="text" name="" placeholder="Enter your User Name" id=""
-                    className="w-full text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none" />
-
-                  <div className="mt-4 h-[42px] self-stretch rounded-[15px] bg-blue_gray-100" />
+      <div className="w-full pb-[13px] bg-gradient">
+        <div>
+           {/* IMA component, styled with flexbox */}
+          <IMA className="flex md:flex-col justify-between items-start gap-5 bg-indigo-900" />
+          {/* Main content container */}
+          <div className="flex md:flex-col justify-center items-start gap-[49px]">
+            {/* Sidebar container */}
+            <div className="h-[825px] w-[17%] md:w-full md:h-auto md:p-5 relative">
+              <div className="flex flex-col items-center mt-[65px] ml-[41px] gap-[25px] md:ml-0">
+                 {/* Additional sidebar content can be added here */}
+              </div>
+               {/* Sidebar buttons */}
+              <div className="w-full h-max left-0 bottom-0 right-0 top-0 m-auto absolute">
+                <div>
+                  <Button shape="square" className="w-full sm:px-5 z-[1] border-black-900 border border-solid">
+                    Home
+                  </Button>
+                  <div className="h-[52px] mt-[-49px] bg-indigo-100 shadow-bs" />
                 </div>
+                {/* Additional sidebar buttons */}
+                <div className="flex flex-col mt-[-52px] py-[52px] md:py-5 bg-indigo-300">
 
-                {/* Password field */}
-                <div className="mt-7 flex flex-col">
-                  <label for="" className="text-neutral-1900 text-base font-normal">Password :</label>
-                  <input type="text" name="" placeholder="Enter your Password" id=""
-                    className="w-full text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none" />
-
-                  <div className="mt-4 h-[42px] self-stretch rounded-[15px] bg-blue_gray-100" />
+                  <Button shape="square" className="w-full sm:px-5">
+                    Teachers
+                  </Button>
+                  <Button shape="square" className="w-full sm:px-5">
+                    Student Details
+                  </Button>
+                  <Button shape="square" className="w-full sm:px-5">
+                    Papers
+                  </Button>
+                  <Button shape="square" className="w-full sm:px-5">
+                    TimeTable
+                  </Button>
+                  <Button
+                    size="s"
+                    as="h5"
+                    className="flex justify-center items-center h-[52px] px-[35px] py-3 sm:px-5 bg-indigo-100 shadow-bs"
+                  >
+                    Payments
+                  </Button>
+              
+                  <div className="h-[52px] mb-[408px] bg-indigo-100 shadow-bs" />
                 </div>
-
-                {/* Login button */}
-                <div className="relative mt-[52px] h-[42px] w-[29%] self-center md:h-auto">
-                  <a href="#" className="mr-6 md:mr-0">
-                    <a href="#" className="mt-[53px] self-center">
-                      <Button as="h2">Login</Button>
-                    </a>
-                  </a>
-                  <div className="absolute bottom-0 left-0 right-0 top-0 m-auto h-[44px] w-full rounded-[15px] bg-blue_gray-100" />
+              </div>
+            </div>
+            {/* Main content area */}
+            <div className="flex flex-col items-end md:self-stretch mt-[3px] gap-5 md:p-5 flex-1">
+              <div className="flex sm:flex-col justify-between items-center w-[79%] md:w-full gap-5">
+               {/* Upcoming Events button */}
+                <Button size="2xl" as="h3" className="self-end mb-2.5 !text-blue_gray-900">
+                  Upcoming Events
+                </Button>
+                {/* Profile, Settings, and Logout buttons */}
+                <div className="flex flex-col">
+                  <Button
+                    as="p"
+                    className="flex justify-center items-center h-[35px] pt-[5px] pb-px px-7 sm:px-5 border-black-900 border border-solid bg-blue_gray-100"
+                  >
+                    My Profile
+                  </Button>
+                  <Button
+                    as="p"
+                    className="flex justify-center items-center h-[35px] px-[35px] py-[3px] sm:px-5 border-black-900 border border-solid bg-blue_gray-100"
+                  >
+                    Settings
+                  </Button>
+                  <Button
+                    as="p"
+                    className="flex justify-center items-center h-[35px] px-[35px] py-[3px] sm:px-5 border-black-900 border border-solid bg-blue_gray-100"
+                  >
+                    Logout
+                  </Button>
                 </div>
-              </form>
+              </div>
+               {/* Events and Calendar section */}
+              <div className="flex md:flex-col self-stretch justify-end items-center mr-[13px] gap-[49px] md:mr-0">
+                <div className="flex flex-col md:self-stretch gap-8 flex-1">
+                  {/* Header for events */}
+                  <div className="flex justify-between gap-5 bg-indigo-A200 shadow-xs rounded-[15px]">
+                    <div className="flex self-end justify-between w-[28%] ml-[31px] gap-5 flex-wrap">
+                      <Heading size="lg" as="h4" className="!text-white-A700">
+                        Date
+                      </Heading>
+                      <Heading size="lg" as="h4" className="!text-white-A700">
+                        Time
+                      </Heading>
+                    </div>
+                    <Heading size="lg" as="h4" className="self-end mr-[23px] !text-white-A700">
+                      Hall No
+                    </Heading>
+                  </div>
+                  <div className="flex sm:flex-col justify-between items-start gap-5 p-1.5 bg-blue-200 shadow-xs rounded-[15px]">
+                    <div className="flex justify-between w-[44%] sm:w-full mb-[5px] ml-[19px] gap-5 md:ml-0 flex-wrap">
+                      <Text size="s" as="p">
+                        10th Feb
+                      </Text>
+                      <Text size="s" as="p">
+                        08.00 AM
+                      </Text>
+                    </div>
+                    <Text size="xs" as="p" className="mr-56 md:mr-0">
+                      2025 First Theory Class
+                    </Text>
+                  </div>
+
+                  {/* Placeholder elements for additional events */}
+                  <div className="h-[39px] bg-blue-200 shadow-xs rounded-[15px]" />
+                  <div className="h-[39px] bg-blue-200 shadow-xs rounded-[15px]" />
+                  <div className="h-[39px] bg-blue-200 shadow-xs rounded-[15px]" />
+                </div>
+                {/* calender i used just an image for the frontend : */}
+                <Img src="images/img_calender_1.png" alt="calenderone_one" className="w-[35%] md:w-full object-cover" />
+              </div>
             </div>
           </div>
         </div>
